5e53fd80a6e761b73722ab790c4745ea4bee2aa5
Dollth-ing <ceceliahingston@gmail.com>
2025-10-02 22:51:44 +1300

    uhhhh

--- LaciSynchroni/UI/CompactUI.cs
+++ LaciSynchroni/UI/CompactUI.cs
@@ -698,7 +698,7 @@ public class CompactUi : WindowMediatorSubscriberBase
             if (unconvertedTextures > 0)
             {
                 UiSharedService.AttachToolTip(
-                    $"You have {unconvertedTextures} texture(s) that are not BCn format. Consider converting them to BC7 to reduce their size." +
+                    $"You have {unconvertedTextures} texture(s) that are not compressed format. Consider converting them to BC7 to reduce their size." +
                     UiSharedService.TooltipSeparator +
                     "Click to open the Character Data Analysis");
             }
